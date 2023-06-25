d12ce8fe6859409abf0633a0f6f6275fe6e6124e
Kristiyan Tenev <kristiyan.tenev@siemens.com>
2023-06-25 23:26:16 +0300

    Add project files.

---
+++ ValidateString/Program.cs
@@ -0,0 +1,28 @@
+﻿static Boolean isUppercase(string s)
+{
+    return s.All(char.IsUpper);
+}
+static Boolean isLowercase(string s)
+{
+    return s.All(char.IsLower);
+}
+static Boolean isDigit(string s)
+{
+    return s.All(char.IsDigit);
+}
+
+static Boolean isLetter(string s)
+{
+    return s.All(char.IsLetter);
+}
+
+static Boolean isPasswordComplex(string s)
+{
+    return s.Any(char.IsUpper) && s.Any(char.IsLower) && s.Any(char.IsDigit) && s.Any(char.IsLetter);
+}
+
+Console.WriteLine(isDigit("Hello"));
+Console.WriteLine(isLetter("Hello"));
+Console.WriteLine(isLowercase("Hello"));
+Console.WriteLine(isUppercase("HELLO"));
+Console.WriteLine(isPasswordComplex("HELLO"));
